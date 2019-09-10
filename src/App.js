bda88f09d01a57b7b9ec6cf4e7d0727903c4d586
maciel <maciel.gaeano@anma.edu.uy>
2019-09-10 00:40:46 -0300

    Se agregan cosas de base de datos

--- src/App.js
+++ src/App.js
@@ -6,11 +6,50 @@ import Nav from './componentes/views/nav';
 import Player from './componentes/views/player';
 
 class App extends Component {
+
+  
+  constructor(props){
+    super(props);
+    this.state = {
+        title: 'busqda de link',
+        link: []
+    }
+  }
+
+  //Make ajax call here
+
+  componentDidMount() {
+    console.log('COMPONENT HAS MOUNTED');
+  }
+
+  addVideo(){
+    let letLink = {
+      link: this.refs.link.value
+    };
+    var request = new request('http://localhost:3001/api/letLink', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type' : 'aplication/json' }),
+      body: JSON.stringify(letLink)
+    });
+    console.log(letLink);
+    //xmlhttprequest()
+    fetch(request)
+      .then(function(respose) {
+        Response.JSON()
+          .then(function(letLink){
+            console.log(letLink);
+          }) 
+    });
+
+  }
+
   render() {
 
-    let videoLink = "https://www.youtube.com/embed/Dukqpeagrp0";
+    let videoLink = this.state.link;
 
     return (
+
+
       <div>
         <Nav></Nav>
         <Player video={videoLink}></Player>
